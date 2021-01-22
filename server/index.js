8c2789a3ea041786099c43d0a07f44d188bf0185
Crhistian Ramirez <cramirez@four51.com>
2021-01-22 14:19:06 -0600

    merge ryans additions

--- server/index.js
+++ server/index.js
@@ -6,12 +6,22 @@ const projectRoot = path.join(__dirname, "../");
 
 // client
 app.use(express.static("client"));
-app.get("/", (req, res) => {
-  res.sendFile(`${projectRoot}/client/index.html`);
+
+app.get("/login", (req, res) => {
+  res.sendFile(`${projectRoot}/client/login/index.html`);
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(`${projectRoot}/client/register/index.html`);
 });
 
 // server
 app.use("/api/example", require("./routes/example"));
+
+// catch-all
+app.get("/*", (req, res) => {
+  res.sendFile(`${projectRoot}/client/login/index.html`);
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
