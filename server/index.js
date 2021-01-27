03ef302b1bba87c710ecc048d18fefc77ee61cf4
Crhistian Ramirez <cramirez@four51.com>
2021-01-27 17:16:06 -0600

    move alexa routes

--- server/index.js
+++ server/index.js
@@ -8,38 +8,8 @@ const projectRoot = path.join(__dirname, "../");
 // client
 app.use(express.static("client"));
 
-// -- Web Authorization URI --
-// The URI where customers will be redirected in the companion app to enter login credentials.
-// https://developer.amazon.com/en-US/docs/alexa/account-linking/configure-implicit-grant.html#overview-of-the-implicit-grant-flow
-app.get("/authorize", (req, res) => {
-  const { 
-    state, 
-    client_id, 
-    response_type, 
-    scope, 
-    redirect_uri
-  } =  req.query;
-  res.sendFile(`${projectRoot}/client/login/index.html`, {
-    headers: {
-      ordercloud_voice_state: state,
-      ordercloud_voice_client_id: client_id,
-      ordercloud_voice_response_type: response_type,
-      ordercloud_voice_scope: scope, 
-      ordercloud_voice_redirect_uri: redirect_uri,
-    }
-  });
-});
-
-// -- Access Token URI --
-// This URI will be used for both access token and token refresh requests.
-// https://developer.amazon.com/en-US/docs/alexa/account-linking/configure-authorization-code-grant.html#tokens
-app.get("/token", (req, res) => {
-  console.log('TOKEN REQUEST', JSON.stringify(req));
-  res.sendFile(`${projectRoot}/client/login/index.html`);
-});
 
 // TODO: Bring this page back if we have time, right now it is not necessary as we will use the same client ID for the entire skill
-//
 // // Hidden route for developers or application owners to register their ClientID with a keyword
 // app.get("/register", (req, res) => {
 //   res.sendFile(`${projectRoot}/client/register/index.html`);
@@ -48,6 +18,7 @@ app.get("/token", (req, res) => {
 // server
 app.use("/api/example", require("./routes/example"));
 app.use('/api/ordercloud', require('./routes/ordercloud'))
+app.use('/api/alexa', require('./routes/alexa'))
 
 // catch-all
 app.get("/*", (req, res) => {
