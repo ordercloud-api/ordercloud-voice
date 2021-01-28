12912659f908af9b7557f98cf22fe50f672b27bd
Crhistian Ramirez <cramirez@four51.com>
2021-01-28 10:28:54 -0600

    add token

--- server/skill/handlers/error-handler.ts
+++ server/skill/handlers/error-handler.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { HandlerInput } from 'ask-sdk-core'
 import { Response } from 'ask-sdk-model'
+import e from 'express'
 
 /**
  * Generic error handling to capture any syntax or routing errors. If you receive an error
@@ -10,12 +12,20 @@ export default {
   canHandle(): boolean {
     return true
   },
-  handle(handlerInput: HandlerInput, error: unknown): Response {
+  handle(handlerInput: HandlerInput, error: any): Response {
     const speakOutput =
       'Sorry, I had trouble doing what you asked. Please try again.'
-    console.log(error)
-    console.log(`~~~~ Error handled: ${JSON.stringify(error)}`)
-
+    console.log(`~~~~ Error handled:`)
+    if (typeof error === 'string') {
+      console.log(error)
+    } else {
+      if (error.isOrderCloudError) {
+        console.log(error.message)
+        console.log(JSON.stringify(error.errors))
+      } else {
+        console.log(JSON.stringify(error))
+      }
+    }
     return handlerInput.responseBuilder
       .speak(speakOutput)
       .reprompt(speakOutput)
